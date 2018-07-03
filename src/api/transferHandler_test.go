d57443160e79183a6603d9e10cbc31eb61150eb6
Apipol Sukgler <apipol@computerlogy.com>
2018-07-04 01:15:54 +0700

    [Edited]: แก้ไขข้อมูลทดสอบ

--- src/api/transferHandler_test.go
+++ src/api/transferHandler_test.go
@@ -11,14 +11,14 @@ import (
 func Test_TransferHandler_Input_giver_oat_receiver_nut_should_be_success(t *testing.T) {
 	url := "/transfer"
 	requestBody := map[string]interface{}{
-		"receiver": "1oat",
-		"giver":    "2nut",
+		"giver":    "123456789",
+		"receiver": "987654321",
 		"amount":   1000,
 	}
 	requestBodyString, _ := json.Marshal(requestBody)
 	request := httptest.NewRequest("POST", url, bytes.NewBuffer(requestBodyString))
 	responseRecorder := httptest.NewRecorder()
-	expected := `{"status":""}`
+	expected := `{"status":"Success","receiver":{"name":"Nut","id":"987654321","amount":6000},"giver":{"name":"Oat","id":"123456789","amount":11000},"fee":0}`
 
 	TransferHandler(responseRecorder, request)
 	response := responseRecorder.Result()
